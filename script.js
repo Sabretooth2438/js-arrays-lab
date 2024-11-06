f8ee716ce1f258cba31a85f3930723789e61f2d0
AbdulRahman Alam <abdulrahmanidrees@gmail.com>
2024-11-06 11:53:59 +0300

    Exercise 5 and 6 is Done

--- script.js
+++ script.js
@@ -11,8 +11,18 @@
 // foods.unshift('tacos')
 // console.log('Exercise 3 result:', foods)
 
-//Exercise 4
+// //Exercise 4
+// foods = ['pizza', 'cheeseburger']
+// foods.unshift('tacos')
+// const favfood = foods[1]
+// console.log('Exercise 4 result:', favfood)
+
+// //Exercise 5
+// foods = ['pizza', 'cheeseburger']
+// foods.splice(1, 0, 'tacos')
+// console.log('Exercise 5 result:', foods)
+
+//Exercise 6
 foods = ['pizza', 'cheeseburger']
-foods.unshift('tacos')
-const favfood = foods[1]
-console.log('Exercise 4 result:', favfood)
+foods.splice(0, 1, 'sushi', 'cupcake')
+console.log('Exercise 6 result:', foods)
