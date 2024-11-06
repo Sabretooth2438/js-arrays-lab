6c48418ce36af09164f8f30ef39a3e7ea360171a
AbdulRahman Alam <abdulrahmanidrees@gmail.com>
2024-11-06 11:27:07 +0300

    Exercise 1-3 is Done

---
+++ script.js
@@ -0,0 +1,13 @@
+// //Exercise 1
+// const foods = []
+// console.log('Exercise 1 result:', foods)
+
+// //Exercise 2
+// foods = ['pizza', 'cheeseburger']
+
+// console.log('Exercise 2 result:', foods)
+
+//Exercise 3
+foods = ['pizza', 'cheeseburger']
+foods.unshift('tacos')
+console.log('Exercise 2 result:', foods)
