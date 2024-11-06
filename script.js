ce79a59c3ea27db32184ebcf3e1150ad7deeab76
AbdulRahman Alam <abdulrahmanidrees@gmail.com>
2024-11-06 11:30:36 +0300

    Exercise 4 is Done

--- script.js
+++ script.js
@@ -4,10 +4,15 @@
 
 // //Exercise 2
 // foods = ['pizza', 'cheeseburger']
-
 // console.log('Exercise 2 result:', foods)
 
-//Exercise 3
+// //Exercise 3
+// foods = ['pizza', 'cheeseburger']
+// foods.unshift('tacos')
+// console.log('Exercise 3 result:', foods)
+
+//Exercise 4
 foods = ['pizza', 'cheeseburger']
 foods.unshift('tacos')
-console.log('Exercise 2 result:', foods)
+const favfood = foods[1]
+console.log('Exercise 4 result:', favfood)
