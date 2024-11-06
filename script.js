17e06d46f4e8c87ccb9928e8e5d20b1d5be9eff8
AbdulRahman Alam <abdulrahmanidrees@gmail.com>
2024-11-06 12:47:56 +0300

    Exercises 7-10 is Done

--- script.js
+++ script.js
@@ -1,28 +1,79 @@
 // //Exercise 1
 // const foods = []
+
 // console.log('Exercise 1 result:', foods)
 
 // //Exercise 2
-// foods = ['pizza', 'cheeseburger']
+// const foods = ['pizza', 'cheeseburger']
+
 // console.log('Exercise 2 result:', foods)
 
 // //Exercise 3
-// foods = ['pizza', 'cheeseburger']
+// const foods = ['pizza', 'cheeseburger']
 // foods.unshift('tacos')
+
 // console.log('Exercise 3 result:', foods)
 
 // //Exercise 4
-// foods = ['pizza', 'cheeseburger']
+// const foods = ['pizza', 'cheeseburger']
 // foods.unshift('tacos')
 // const favfood = foods[1]
+
 // console.log('Exercise 4 result:', favfood)
 
 // //Exercise 5
-// foods = ['pizza', 'cheeseburger']
-// foods.splice(1, 0, 'tacos')
+// const foods = ['pizza', 'cheeseburger']
+// foods.unshift('tacos')
+// foods.splice(2, 0, 'tofu')
+
 // console.log('Exercise 5 result:', foods)
 
-//Exercise 6
-foods = ['pizza', 'cheeseburger']
-foods.splice(0, 1, 'sushi', 'cupcake')
-console.log('Exercise 6 result:', foods)
+// //Exercise 6
+// const foods = ['pizza', 'cheeseburger']
+// foods.unshift('tacos')
+// foods.splice(2, 0, 'tofu')
+// foods.splice(1, 1, 'sushi', 'cupcake')
+
+// console.log('Exercise 6 result:', foods)
+
+// //Exercise 7
+// const foods = ['pizza', 'cheeseburger']
+// foods.unshift('tacos')
+// foods.splice(2, 0, 'tofu')
+// foods.splice(1, 1, 'sushi', 'cupcake')
+// const yummy = foods.slice(1, 3)
+
+// console.log('Exercise 7 result:', yummy)
+
+// //Exercise 8
+// const foods = ['pizza', 'cheeseburger']
+// foods.unshift('tacos')
+// foods.splice(2, 0, 'tofu')
+// foods.splice(1, 1, 'sushi', 'cupcake')
+// const yummy = foods.slice(1, 3)
+// const soyIdx = foods.indexOf('tofu')
+
+// console.log('Exercise 8 result:', soyIdx)
+
+// //Exercise 9
+// const foods = ['pizza', 'cheeseburger']
+// foods.unshift('tacos')
+// foods.splice(2, 0, 'tofu')
+// foods.splice(1, 1, 'sushi', 'cupcake')
+// const yummy = foods.slice(1, 3)
+// const soyIdx = foods.indexOf('tofu')
+// const allFoods = foods.join(' -> ')
+
+// console.log('Exercise 9 result:', allFoods)
+
+// //Exercise 10
+// const foods = ['pizza', 'cheeseburger']
+// foods.unshift('tacos')
+// foods.splice(2, 0, 'tofu')
+// foods.splice(1, 1, 'sushi', 'cupcake')
+// const yummy = foods.slice(1, 3)
+// const soyIdx = foods.indexOf('tofu')
+// const allFoods = foods.join(' -> ')
+// const hasSoup = foods.includes('soup')
+
+// console.log('Exercise 10 result:', hasSoup)
